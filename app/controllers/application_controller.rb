5a7c5ad1677c81ba36e151d6a4d1991a9ad1cb9e
Tahmid Shakil <md.ahnaf.tahmid.shakil@framgia.com>
2016-08-24 17:54:40 +0600

    Added CORS support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  before_action :set_headers
+
+  private
+  def set_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
 end
